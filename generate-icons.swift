c890e34f264a268f006ff211d066bab016fc1d08
Max Chuquimia <2460248+maxchuquimia@users.noreply.github.com>
2022-03-26 14:00:02 +1100

    Update generate-icons.swift

--- generate-icons.swift
+++ generate-icons.swift
@@ -1,9 +1,8 @@
 import Cocoa
 
-guard CommandLine.arguments.count == 3 else { 
-
-	print("Usage: swift \(CommandLine.arguments[0]) <path/to/largeIcon.png> <path/to/Assets.xcassets/AppIcon.appiconset>")
-	exit(1)
+guard CommandLine.arguments.count == 3 else {
+    print("Usage: swift \(CommandLine.arguments[0]) <path/to/largeIcon.png> <path/to/Assets.xcassets/AppIcon.appiconset>")
+    exit(1)
 }
 
 let inputImagePath = CommandLine.arguments[1]
@@ -20,39 +19,38 @@ func resize(image: NSImage, w: Float, h: Float) -> Data {
     let newImage = NSImage(size: destSize)
     newImage.lockFocus()
     image.draw(
-		in: NSMakeRect(0, 0, destSize.width, destSize.height), 
-		from: NSMakeRect(0, 0, image.size.width, image.size.height), 
-		operation: .sourceOver, 
-		fraction: 1
-	)
+        in: NSMakeRect(0, 0, destSize.width, destSize.height),
+        from: NSMakeRect(0, 0, image.size.width, image.size.height),
+        operation: .sourceOver,
+        fraction: 1
+    )
     newImage.unlockFocus()
     newImage.size = destSize
-	print(destSize)
+    print(destSize)
     return NSBitmapImageRep(data: newImage.tiffRepresentation!)!.representation(using: .png, properties: [:])!
 }
 
-var images = (iconSet["images"] as! [[String: String]]) 
+var images = (iconSet["images"] as! [[String: String]])
 images.enumerated().forEach { data in
-	
-	let scaleString = data.1["scale"]!
-	let nameString = data.1["idiom"]!
-	let sizeString = data.1["size"]!
+    let scaleString = data.1["scale"]!
+    let nameString = data.1["idiom"]!
+    let sizeString = data.1["size"]!
 
-	let scale = Float(scaleString.replacingOccurrences(of: "x", with: ""))!
-	let size = Float(sizeString.components(separatedBy: "x").first!)!
+    let scale = Float(scaleString.replacingOccurrences(of: "x", with: ""))!
+    let size = Float(sizeString.components(separatedBy: "x").first!)!
 
-	// The Retina display affects scaling. Account for this.
-	let screenFactor = Float(NSScreen.main!.backingScaleFactor)
-	let side =  size * scale / screenFactor
+    // The Retina display affects scaling. Account for this.
+    let screenFactor = Float(NSScreen.main!.backingScaleFactor)
+    let side =  size * scale / screenFactor
 
-	print("Scaling \(scale)*\(size) = \(size * scale)")
+    print("Scaling \(scale)*\(size) = \(size * scale)")
 
-	let resizedImage = resize(image: imageTemplate, w: side, h: side)
-	
-	let filename = "\(nameString)_\(sizeString)@\(scaleString).png"
-	let path = URL(fileURLWithPath: inputIconSetPath + "/" + filename)
-	try! resizedImage.write(to: path)
-	images[data.0]["filename"] = filename
+    let resizedImage = resize(image: imageTemplate, w: side, h: side)
+
+    let filename = "\(nameString)_\(sizeString)@\(scaleString).png"
+    let path = URL(fileURLWithPath: inputIconSetPath + "/" + filename)
+    try! resizedImage.write(to: path)
+    images[data.0]["filename"] = filename
 }
 
 iconSet["images"] = images
@@ -61,4 +59,3 @@ let newIconSetData = try! JSONSerialization.data(withJSONObject: iconSet, option
 try! newIconSetData.write(to: iconSetJSONURL)
 
 print("Done")
-
